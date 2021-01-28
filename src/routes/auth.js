9e53e2bc7308979188f3e03a0eff3feb54a2b9dc
arvind8998 <asangwan8998@gmail.com>
2021-01-28 14:38:06 +0530

    Added cart api's

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const {signup, signIn}  = require('../controllers/auth')
-const {requireSignin} = require('../common-middleware/index')
-const {check} = require('express-validator');
-const {validateSignupRequest, validateSigninRequest, isRequestValidated} = require('../Validators/auth')
+const express = require("express")
+const router = express.Router()
+const { signup, signIn, signout } = require("../controllers/auth")
+const { requireSignin } = require("../common-middleware/index")
+const { check } = require("express-validator")
+const {
+  validateSignupRequest,
+  validateSigninRequest,
+  isRequestValidated,
+} = require("../Validators/auth")
 
-router.post('/signup',validateSignupRequest, isRequestValidated,signup);
-router.post('/signin',validateSigninRequest,isRequestValidated, signIn);
+router.post("/signup", validateSignupRequest, isRequestValidated, signup)
+router.post("/signin", validateSigninRequest, isRequestValidated, signIn)
 
-router.post('/profile', requireSignin, (req,res)=>{
-    res.status(200).json({user: req.user})
+router.post("/signout", requireSignin, signout)
+router.post("/profile", requireSignin, (req, res) => {
+  res.status(200).json({ user: req.user })
 })
 
 module.exports = router
